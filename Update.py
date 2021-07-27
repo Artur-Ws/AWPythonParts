40a000a6c6b872bd0acf0f47a4d35311b5463fb8
AW <artur.wysocki@gmail.com>
2021-07-27 16:37:02 +0200

    Added copyFiles method

--- Update.py
+++ Update.py
@@ -1,12 +1,18 @@
+import os
 
-
-sourcePath = "\\ALPLAN\alplan\AWPythonPartsCopySource"
-targetPath = "C:\ProgramData\Nemetschek\Allplan\2021\Etc\PythonPartsScripts\ToolsAndStartExamples"
-class Update:
+sourcePath = "\\ALPLAN\\alplan\\AWPythonPartsCopySource"
+targetPath = "C:\\ProgramData\\Nemetschek\\Allplan\\2021\\Etc\\PythonPartsScripts\\ToolsAndStartExamples"
+class UpdatePP:
     def __init__(self):
         pass
 
     def searchSourcePath(self):
+        pass
+
+    def copyFiles(self):
+
+        for item in os.listdir()
 
 
-        
+
+

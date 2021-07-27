72020ddf2986af7e7d5c53729ea8bf8b6c1dd3cb
Artur Wysocki <artur.wysocki@pekabex.poznan>
2021-07-27 15:20:13 +0200

    Added source and target path to Update.py

--- Update.py
+++ Update.py
@@ -1,7 +1,7 @@
 
 
-
-
+sourcePath = "\\ALPLAN\alplan\AWPythonPartsCopySource"
+targetPath = "C:\ProgramData\Nemetschek\Allplan\2021\Etc\PythonPartsScripts\ToolsAndStartExamples"
 class Update:
     def __init__(self):
         pass

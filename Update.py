1737d9cc0c2ec888712dd1ca88b9c5ed45333152
AW <artur.wysocki@gmail.com>
2021-08-23 11:44:27 +0200

    Added copy directory to Update.py with distutils.dir_util.copy_tree

--- Update.py
+++ Update.py
@@ -1,14 +1,16 @@
 import os
 import tkinter
+import shutil
+from distutils.dir_util import copy_tree
 
-
-#sourcePath = "\\ALPLAN\\alplan\\AWPythonPartsCopySource"
-sourcePath = "C:\\ProgramData\\Nemetschek\\Allplan\\2021\\Etc\\PythonPartsScripts\\AWPythonParts"
+# sourcePath = "C:\\ProgramData\\Nemetschek\\Allplan\\2021\\Etc\\PythonPartsScripts\\AWPythonParts\\copysource"
+sourcePath = "\\ALPLAN\\alplan\\AWPythonPartsCopySource"
 targetPath = "C:\\ProgramData\\Nemetschek\\Allplan\\2021\\Etc\\PythonPartsScripts\\AWPythonParts\\aaa"
 
-def check_allplan_version(build_ele, version):
 
+def check_allplan_version(build_ele, version):
     return True
+
 
 def create_element(build_ele, doc):
     model_elem_list = []
@@ -18,15 +20,16 @@ def create_element(build_ele, doc):
 
     return (model_elem_list, handle_list)
 
+
 print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 if not os.path.exists(targetPath):
     os.makedirs(targetPath)
+
+copy_tree(sourcePath, targetPath)
 
 window = tkinter.Tk()
 window.title("PBX PP Update")
 info = tkinter.Label(window, text='Pomyślnie zaktualizowano PythonPartsy', width=50, height=5)
 info.grid(column=0, row=0)
 window.mainloop()
-
-

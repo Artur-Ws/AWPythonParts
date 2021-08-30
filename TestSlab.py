066a695e7efa87d828acf4551c31884f7f8da565
AW <artur.wysocki@gmail.com>
2021-08-30 15:08:08 +0200

    Primitive working version of TestSlab that creates a 3D obiect 1mx1mx1m

--- TestSlab.py
+++ TestSlab.py
@@ -1,29 +1,51 @@
 import os
 import tkinter
-
-
-
+import NemAll_Python_Geometry as AllplanGeo
+import NemAll_Python_BaseElements as AllplanBaseElements
+import NemAll_Python_BasisElements as AllplanBasisElements
 
 def check_allplan_version(build_ele, version):
 
     return True
 
 def create_element(build_ele, doc):
-    model_elem_list = []
-    handle_list = []
-    del build_ele
-    del doc
 
-    return (model_elem_list, handle_list)
+    element = Slab(doc)
+
+    return element.create(build_ele)
 
 print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 
+class Slab():
 
-# window = tkinter.Tk()
-# window.title("PBX PP Update")
-# info = tkinter.Label(window, text='Pomyślnie zaktualizowano PythonPartsy')
-# info.grid(column=0, row=0)
-# window.mainloop()
+    def __init__(self, doc):
+        self.model_ele_list = []
+        self.handle_list = []
+        self.document = doc
+
+    def create(self,build_ele):
+        self.create_slab(build_ele)
+        return(self.model_ele_list, self.handle_list)
+
+### "Name" in .pyp file is reference to parameter when create an element
+
+    def create_slab(self, build_ele):
+
+        slab = AllplanGeo.Polyhedron3D.CreateCuboid(1000,1000,1000)
+
+        com_prop = AllplanBaseElements.CommonProperties()
+        com_prop.GetGlobalProperties()
+        slab_obiect = AllplanBasisElements.ModelElement3D(com_prop, slab)
+
+        return self.model_ele_list.append(slab_obiect)
 
 
+
+
+########################################################################################################################
+window = tkinter.Tk()
+window.title("Non-crashed")
+info = tkinter.Label(window, text='Jeśli widzisz to okno to wszystko raczej działa')
+info.grid(column=0, row=0)
+window.mainloop()
---
+++ pypFiles/TestSlab.pyp
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Element>
+    <Script>
+        <Name>AWPythonParts\TestSlab.py</Name>
+        <Title>Balkon-Test</Title>
+        <Version>1.0</Version>
+    </Script>
+    <Page>
+        <Name>Page1</Name>
+        <Text>BalkonPP</Text>
+    </Page>
+
+</Element>

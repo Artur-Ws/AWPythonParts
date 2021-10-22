87d174f17be58ae88e2456212d94063c6e01cd6c
AW <artur.wysocki@gmail.com>
2021-10-22 11:23:43 +0200

    Final version of AutoBackup.py for tests

---
+++ AutoBackup/test.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter import messagebox
+import time
+import datetime
+
+x= str(datetime.datetime.now())[0:10]
+print(x)

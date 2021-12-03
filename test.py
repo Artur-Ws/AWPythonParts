c996b1bd1ba59083fd9bde0f8b015acb72017ac8
Artur-Ws <wysocki.a97@gmail.com>
2021-12-03 15:19:38 +0100

    Upgraded Allback.py (now version 1.1) - added filename and break info shown in command window

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-x = '12345'
-print(x[-3:])
+import sys
+
+sys.path.append('C:\Users\artur.wysocki\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\local-packages\Python39\Scripts')
